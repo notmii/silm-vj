9fe7bcbe6861b930bc99b2284f76b0da19866c2a
notmii <notmii@notmii.com>
2021-07-03 18:47:04 +0800

    Twig layout templates.

--- app/public/index.php
+++ app/public/index.php
@@ -12,21 +12,20 @@ use Psr\Http\Message\ServerRequestInterface;
 require __DIR__ . '/../vendor/autoload.php';
 
 define('ROOT', __DIR__.'/..');
-define('TEMPLATES', ROOT.'/src/templates');
+define('TEMPLATES', ROOT.'/src/Templates');
 
 $container = new Container();
-AppFactory::setContainer($container);
-$app = AppFactory::create();
 
 $container->set(\Twig\Environment::class, function() {
   $loader = new \Twig\Loader\FilesystemLoader(TEMPLATES);
+  $loader->addPath(TEMPLATES.'/Layouts', 'layouts');
   return new \Twig\Environment($loader, [
       'cache' => false,// ROOT.'/tmp/cache/twig',
   ]);
 });
 
-// Add Twig-View Middleware
+AppFactory::setContainer($container);
+$app = AppFactory::create();
 $app->get('/', [HomeController::class, 'index']);
 $app->get('/auth', [LoginController::class, 'index']);
-
 $app->run();
